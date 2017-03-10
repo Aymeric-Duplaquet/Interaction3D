f50ed40570d76343f6051cbb975498ee645f8d94
Aymeric-Duplaquet <Aymeric.duplaquet@club-internet.fr>
2017-03-10 17:16:37 -0500

    Balise à regarder, prefab Environement, centre de l'écrant marqué par un point blanc

---
+++ Assets/Script/BaliseColor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaliseColor : MonoBehaviour {
+
+    Material mat;
+    public float validateTime;
+    private float actualValidateTime;
+
+	// Use this for initialization
+	void Start () {
+        mat = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+
+    public void uptadeColor(float deltaTime)
+    {
+        actualValidateTime += deltaTime;
+        float green = (actualValidateTime / validateTime);
+        float red = 1 - green;
+
+        GetComponent<Renderer>().material.color = new Color(red, green, 0);
+
+        if(actualValidateTime >= validateTime)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Script/BaliseScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaliseScript : MonoBehaviour {
+
+    public float timeToUpdate;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            BaliseColor bal = hit.transform.GetComponent<BaliseColor>();
+            if (bal != null)
+            {
+                bal.uptadeColor(Time.deltaTime);
+            }
+           
+        }
+        else
+        {
+
+        }
+           
+
+    }
+}
